4427fda448fe9385a90c8d0e80e2dc8a27500123
AnaSaless <166721815+AnaSaless@users.noreply.github.com>
2025-10-22 19:12:44 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,29 @@
 
 public class Main {
 
+    // Método principal, onde o programa começa.
     public static void main(String[] args) {
         System.out.println("=== INICIANDO TESTES DA ARVORE MORSE ===");
-
+        
+// Cria uma nova árvore Morse vazia.
         ArvoreBinariaMorse arvore = new ArvoreBinariaMorse();
 
+        // Preenche a árvore com o alfabeto Morse padrão.
         System.out.println("Arvore com o alfabeto morse...");
         arvore.popularArvore();
         System.out.println("Arvore populada.");
 
-        arvore.exibir();
-        arvore.exibirHierarquia();
+        arvore.exibir();// Mostra em formato de lista (Letra: Codigo)
+        arvore.exibirHierarquia();// Mostra o desenho da árvore "deitada"
 
+
+        //Testa a decodificação (método buscar).
         System.out.println("Testando o metodo buscar:");
         System.out.println("Buscando codigo '...': " + arvore.buscar("..."));
         System.out.println("Buscando codigo '---': " + arvore.buscar("---"));
         System.out.println("Buscando mensagem '... --- ...': " + arvore.buscar("... --- ..."));
 
+        // Testa a remoção.
         System.out.println("Testando o metodo remover");
         System.out.println("Removendo a letra 'S' (codigo '...')...");
         boolean removeu = arvore.remover("...");
@@ -29,10 +35,12 @@ public class Main {
             System.out.println("Falha na remocao.");
         }
 
+        //Verifica se a letra "S" foi realmente removida.
         System.out.println("Buscando novamente o codigo '...': '" + arvore.buscar("...") + "' (esperado: vazio)");
         System.out.println("Verificando se o caminho para 'H' ('....') foi preservado...");
         System.out.println("Buscando codigo '....': " + arvore.buscar("...."));
 
+        //Testa inserir a letra 'S' de volta.
         System.out.println("\n5. Inserindo 'S' novamente...");
         arvore.inserir("...", 'S');
         System.out.println("Buscando codigo '...': " + arvore.buscar("..."));
